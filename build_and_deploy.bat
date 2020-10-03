bd50d36d08c383946f398981b31278e7bc64a42a
Olegk Sotnikov <olegk_sotnikov@gmx.com>
2020-10-03 20:56:04 +0100

    fixed scripts

--- build_and_deploy.bat
+++ build_and_deploy.bat
@@ -4,5 +4,5 @@ set AWS_PROFILE=myprofile
 echo. & echo *********** Build UI ************* & echo.
 call ng build --prod
 echo. & echo *********** Upload front end to s3 ************* & echo.
-aws s3 sync --exclude "index.html" --exclude "favicon.ico" --metadata-directive REPLACE --content-encoding "gzip" ../stock-monitor-angular2-front-end/dist s3://osotnikov-stock-monitor-front-end-resources-s3-bucket
-aws s3 sync --exclude "*" --include "index.html" --include "favicon.ico" ../stock-monitor-angular2-front-end/dist s3://osotnikov-stock-monitor-front-end-resources-s3-bucket
+aws s3 sync --exclude "index.html" --exclude "favicon.ico" --metadata-directive REPLACE --content-encoding "gzip" dist s3://%npm_package_config_frontEndBucketName%
+aws s3 sync --exclude "*" --include "index.html" --include "favicon.ico" dist s3://%npm_package_config_frontEndBucketName%
